26808d5968008bd0014a5833d077583a36bf5e2b
Brian Gee <briangeertsma@gmail.com>
2022-08-03 13:18:43 -0400

    basic file writing added

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
+import fs from "fs";
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -21,25 +22,30 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+        }
+        console.log("File created successfully");
+    })
+
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-  .prompt([
-    /* Pass your questions in here */
-    ...questions
-
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
+    console.log("Welcome to Brian's README Generator V1.0");
+    inquirer.prompt(questions).then((answers) => {
     console.log(answers);
+    writeToFile("README.md", "test")
   })
   .catch((error) => {
     if (error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
+      console.log("Prompt couldn't be rendered in the current environment");
     } else {
       // Something else went wrong
+      console.log("Something else went wrong");
     }
   });
 }
